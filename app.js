8ac9f587ef6cc1bf7c19c09f61c15bd2d8bc98dd
Martin Mikulka <martin.mikulka@gmail.com>
2016-09-28 20:44:51 +0200

    Switched to handlebars templating engine

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var express = require('express');
+var exphbs  = require('express-handlebars');
 var app = express();
 
 app.use(express.static('public'));
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }));
+app.set('view engine', 'hbs');
 
 var user = require('./routes/user');
 app.use('/user', user);
 
 app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/index.html');
+	res.render('index');
 });
 
 
