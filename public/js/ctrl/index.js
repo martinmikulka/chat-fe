ce56df87b99957893862bdf1905758ec686dfa86
Martin Mikulka <martin.mikulka@gmail.com>
2016-09-28 14:43:26 +0200

    Added login form and basic fe controller

---
+++ public/js/ctrl/index.js
@@ -0,0 +1,17 @@
+var App = App || {};
+App.Ctrl = App.Ctrl || {};
+
+App.Ctrl.Index = function () {
+	this.formLogin = $('#FormLogin');
+	this.formLogin.on('submit', this.login.bind(this));
+};
+
+App.Ctrl.Index.prototype.login = function (ev) {
+	var url = this.formLogin.attr('action') || '/';
+	var method = this.formLogin.attr('method') || 'get';
+	$.ajax({
+		url: url,
+		method: method
+	});
+	return false;
+}
