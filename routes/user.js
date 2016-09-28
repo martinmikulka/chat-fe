97fe0487fd2725a0c648487b294dec327b044f57
Martin Mikulka <martin.mikulka@gmail.com>
2016-09-28 15:53:38 +0200

    Bodyparser added to user routes

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 
+var parser = bodyParser.urlencoded({extended: false});
 var user = require('../controllers/user');
 
-router.post('/', user.register);
-router.post('/login', user.login);
+router.post('/', parser, user.register);
+router.post('/login', parser, user.login);
 
 module.exports = router;
