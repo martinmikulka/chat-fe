f4bb36cab065c191890ea33aa73fccb728e2738b
Martin Mikulka <martin.mikulka@gmail.com>
2016-10-02 13:45:26 +0200

    Added chat functionality

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }));
 app.set('view engine', 'hbs');
 
+
+/**
+ * Routes
+ */
 var user = require('./routes/user');
 app.use('/user', user);
 
@@ -18,7 +24,19 @@ app.get('/chat', function (req, res) {
 });
 
 
+/**
+ * Socket.io related functionality
+ */
+io.on('connection', function (socket) {
+	socket.on('chat_message', function (message) {
+		socket.broadcast.emit('chat_message', message);
+	});
+});
 
-app.listen(3000, function () {
+
+/**
+ * Create server
+ */
+http.listen(3000, function () {
 	console.log('Server is listening on port 3000.');
 });
--- public/js/ctrl/chat.js
+++ public/js/ctrl/chat.js
@@ -1,5 +1,42 @@
 var App = App || {};
 App.Ctrl = App.Ctrl || {};
 
-App.Ctrl.Chat = function () {
+App.Ctrl.Chat = function (opts) {
+	this.socket;
+	this.elements;
+	this.init(opts);
+};
+
+App.Ctrl.Chat.prototype.init = function (opts) {
+	this.socket = io();
+	this.socket.on('chat_message', this.displayMessage.bind(this));
+
+	this.elements = {
+		container: $('#' + opts.container),
+		window: $('<div id="ChatWindow"></div>'),
+		input: $('<form id="ChatInput" autocomplete="off"><input type="text" name="message" /></form>')
+	};
+
+	this.elements.container
+		.append(this.elements.window)
+		.append(this.elements.input);
+
+	this.elements.input.on('submit', this.sendMessage.bind(this));
+	this.elements.input.find('[name="message"]').get(0).focus();
+};
+
+App.Ctrl.Chat.prototype.sendMessage = function (ev) {
+	var $input = this.elements.input.find('[name="message"]');
+	this.socket.emit('chat_message', $input.val());
+	this.displayMessage($input.val(), true);
+	$input.val('');
+	return false;
+};
+
+App.Ctrl.Chat.prototype.displayMessage = function (message, local) {
+	message = message.replace(/\:(-)?\)/g, '<img src="/img/smile.png" />');
+	$('<p>' + message + '</p>')
+		.addClass('message')
+		.addClass(local ? 'local' : 'public')
+		.appendTo(this.elements.window);
 };
